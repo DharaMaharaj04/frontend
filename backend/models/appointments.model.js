4ac11c67033c0f4b43ede0bbf7ec591c8bfc4b1a
DharaMaharaj04 <maharajdhara95@gmail.com>
2022-05-30 11:37:11 +0530

    calendar

---
+++ backend/models/appointments.model.js
@@ -0,0 +1,22 @@
+const moongose = require('mongoose');
+
+const Schema = moongose.Schema;
+
+const appointmentSchema = new Schema({
+  title: {type: String},
+  img: {type: String},
+  email: {type: String},
+  number: {type: String},
+  start: {type: String},
+  fromTo: {type: String},
+  doctor: {type: String},
+  injury: {type: String},
+  from: {type: String},
+  to: {type: String},
+  },
+  { timestamps: true,
+});
+
+const Appointment = moongose.model('Appointment', appointmentSchema);
+
+module.exports = Appointment;
---
+++ frontend/src/interfaces/page.ts
@@ -0,0 +1,13 @@
+export interface IPageData {
+  title?: string;
+  loaded?: boolean;
+  breadcrumbs?: IBreadcrumb[];
+  fulFilled?: boolean;
+}
+
+export interface IBreadcrumb {
+  title: string;
+  route?: string;
+  name?: string;
+  color?: string;
+}
